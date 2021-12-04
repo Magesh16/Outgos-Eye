1dd147f1c86a8485d5b29c9f134e154b4c8cff0b
Magesh16 <mageshsk16@gmail.com>
2021-12-04 19:59:50 +0530

    Add data in localstorage

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -20,6 +20,7 @@ const Form = () =>{
     const {addTransaction} = useContext(ExpenseTrackerContext);
     const {segment} = useSpeechContext();
     const createTransaction = () =>{
+        if (Number.isNaN(Number(formData.amount)) || !formData.date.includes('-')) return;
         const transaction = { ...formData, amount: Number(formData.amount), id:uuidv4()}; 
         addTransaction(transaction);
         setFormData(initialState);
